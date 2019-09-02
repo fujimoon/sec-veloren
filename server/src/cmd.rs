Luraktinus/veloren

Added unwrap_or to obj_str_opt in handle_object in case we don't return early in the _ => match arm

server/src/cmd.rs
@@ -577,7 +577,10 @@ fn handle_object(server: &mut Server, entity: EcsEntity, args: String, _action: 
             .build();
         server.clients.notify(
             entity,
-            ServerMsg::chat(format!("Spawned: {}", obj_str_opt.unwrap())),
+            ServerMsg::chat(format!(
+                "Spawned: {}",
+                obj_str_opt.unwrap_or("<Unknown object>")
+            )),
         );
     } else {
         server

fujimoon/sec-veloren

server/src/cmd.rs
@@ -627,7 +627,10 @@ fn handle_object(server: &mut Server, entity: EcsEntity, args: String, _action: 
             .build();
         server.clients.notify(
             entity,
-            ServerMsg::private(format!("Spawned: {}", obj_str_res.unwrap())),
+            ServerMsg::private(format!(
+                "Spawned: {}",
+                obj_str_res.unwrap_or("<Unknown object>")
+            )),
         );
     } else {
         server