6a666793d5315e9ec34df76d9280ceaa6f307290
Joshua Barretto <joshua@jsbarretto.com>
2026-07-12 19:36:35 +0100

    Simplified LoD voxel code by using camera-rel coordinates

--- assets/voxygen/shaders/include/lod.glsl
+++ assets/voxygen/shaders/include/lod.glsl
@@ -383,7 +383,7 @@ float lod_voxel_noise(vec3 voxel_pos, float voxel_sz, vec3 f_norm) {
 }
 
 void lod_voxels(vec3 f_pos, vec3 f_norm, vec3 cam_to_frag, out vec3 voxel_pos, out vec3 voxel_norm, out float voxel_sz, out float f_ao) {
-    voxel_pos = f_pos + focus_off.xyz;
+    voxel_pos = f_pos;
     voxel_norm = f_norm;
     voxel_sz = 1.0;
     f_ao = 1.0;
@@ -391,9 +391,8 @@ void lod_voxels(vec3 f_pos, vec3 f_norm, vec3 cam_to_frag, out vec3 voxel_pos, o
     #ifndef EXPERIMENTAL_NOLODVOXELS
         const float VOXEL_SCALE_FACTOR = 30.0;
         vec3 cam_dir = cam_to_frag;
-        vec3 wpos = f_pos + focus_off.xyz;
         
-        voxel_sz = clamp(exp(floor(log(distance(cam_pos.xy, f_pos.xy) * 0.0001 + noise_2d(wpos.xy * 0.01) * 0.02) * 3) / 3) * VOXEL_SCALE_FACTOR, 1.0, 128.0);
+        voxel_sz = clamp(exp(floor(log(distance(cam_pos.xy, f_pos.xy) * 0.0001 + noise_2d((f_pos.xy + focus_off.xy) * 0.01) * 0.02) * 3) / 3) * VOXEL_SCALE_FACTOR, 1.0, 128.0);
         
         #ifdef EXPERIMENTAL_PROCEDURALLODDETAIL
             const float MARCH_THRESHOLD = 4.0;
@@ -403,21 +402,19 @@ void lod_voxels(vec3 f_pos, vec3 f_norm, vec3 cam_to_frag, out vec3 voxel_pos, o
         
         float t = -MARCH_THRESHOLD * voxel_sz;
         while (t < MARCH_THRESHOLD * voxel_sz) {
-            vec3 deltas = (step(vec3(0), -cam_dir * voxel_sz) - fract((wpos - cam_dir * t) / voxel_sz)) / -cam_dir * voxel_sz;
-            float m = min(min(deltas.x, deltas.y), deltas.z);
+            vec3 deltas = (fract((f_pos - cam_dir * t) / voxel_sz) - step(vec3(0), -cam_dir * voxel_sz)) / cam_dir * voxel_sz;
+            t += max(min(min(deltas.x, deltas.y), deltas.z), 0.00001);
 
-            t += max(m, 0.01);
-
-            voxel_pos = (floor((wpos - cam_dir * t) / voxel_sz) + 0.5) * voxel_sz;
+            voxel_pos = (floor((f_pos - cam_dir * t) / voxel_sz) + 0.5) * voxel_sz;
             #ifdef EXPERIMENTAL_PROCEDURALLODDETAIL
-                float surf_depth = lod_voxel_noise(voxel_pos, voxel_sz, f_norm);
+                float surf_depth = lod_voxel_noise(voxel_pos + focus_off.xyz, voxel_sz, f_norm);
             #else
                 const float surf_depth = 0.0;
             #endif
-            if (dot(voxel_pos - wpos, -f_norm) < surf_depth) {
-                vec3 to_center = abs(voxel_pos - (wpos - cam_dir * t));
+            if (dot(voxel_pos - f_pos, -f_norm) < surf_depth) {
+                vec3 to_center = abs(voxel_pos - f_pos + cam_dir * t);
                 voxel_norm = step(max(max(to_center.x, to_center.y), to_center.z), to_center) * sign(-cam_dir);
-                float dist = dot(f_norm * surf_depth - cam_dir * t, f_norm);
+                float dist = dot(cam_dir * t, -f_norm) + surf_depth;
                 f_ao = clamp(dist / voxel_sz * 0.5 + 0.5, 0.0, 1.0);
                 break;
             }
